aea358779c0b985ac959dbc07d03593b106329b7
psubovsky <pablosubovsky@psubovsky.com>
2021-12-01 11:59:49 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('port',3000)
+app.listen(app.get('port'),() => console.log ("Server is up"));
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+app.use(express.static(path.resolve(__dirname,'public')));

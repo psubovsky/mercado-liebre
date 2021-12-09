031a04c10e48eaf28d6a81be4ac83afada38fc73
psubovsky <pablosubovsky@psubovsky.com>
2021-12-09 19:38:04 -0300

    Subido a Heroku

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set('port',3000)
+app.set('port',process.env.PORT || 3000)
 app.listen(app.get('port'),() => console.log ("Server is up"));
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
 app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'./views/login.html')))
 app.use(express.static(path.resolve(__dirname,'public')));

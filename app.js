7e023c186bca945e66af7296d28626da7e471397
psubovsky <pablosubovsky@gmail.com>
2021-12-09 16:02:05 -0300

    Tercer commit

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const app = express();
 app.set('port',3000)
 app.listen(app.get('port'),() => console.log ("Server is up"));
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')))
 app.use(express.static(path.resolve(__dirname,'public')));
